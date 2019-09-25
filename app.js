6eb50e0561f8a69bbe3a6c1649e36f4738654e54
noha nomier <nnomier@gmail.com>
2019-09-25 20:39:44 +0200

    add procfile

--- app.js
+++ app.js
@@ -86,6 +86,11 @@ const title=results.title;
 
 });
 
-app.listen(3000, function() {
-  console.log("running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started ");
 });
